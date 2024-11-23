e8ad2aa31287fd416becc65f9276126e558308ce
Alex Tran <tranduclong092@gmail.com>
2024-11-23 11:43:25 +0700

    Feat: add 2 demo routes

--- demo/server.js
+++ demo/server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// routes
+app.get('/', (req, res) => {
+  console.log('GET /');
+  res.sendStatus(200);
+});
+
+app.get('/api', (req, res) => {
+  console.log('GET /api');
+  // res.sendStatus(200);
+  res.send('Hello world');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
