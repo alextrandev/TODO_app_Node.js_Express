474ad50157d5574abe098426d13464d7a9c84eeb
Alex Tran <tranduclong092@gmail.com>
2024-11-28 14:10:38 +0700

    Feat: add a website route to serve the index page

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 5004;
+
+// get file path
+const __filename = fileURLToPath(import.meta.url);
+// get directory name from path
+const __dirname = dirname(__filename);
+
+// Middleware
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Website routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Start the server
 app.listen(PORT, () => {
