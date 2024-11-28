5b1a319029afd8edaf9939f0a4e0c258f02172f8
Alex Tran <tranduclong092@gmail.com>
2024-11-28 16:35:35 +0700

    Feat: Add user registration functionality and default todo task

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,13 +8,32 @@ const router = express.Router();
 // register as new user
 router.post('/register', async (req, res) => {
   const {username, password} = req.body;
+  // hash the password
+  const hashedPassword = bcrypts.hashSync(password, 8);
 
   // save to db
   try {
-    // hash the password
-    const hashedPassword = bcrypts.hashSync(password, 8);
-    console.log(username, hashedPassword);
-    res.sendStatus(201);
+    const insertUser = db.prepare(`
+      INSERT INTO users (username, password)
+      VALUES (?, ?)
+    `);
+    const result = insertUser.run(username, hashedPassword);
+
+    // add a default todo task
+    const defaultTodo = "Add your first todo task!";
+    const insertTodo = db.prepare(`
+      INSERT INTO todos (task, user_id)
+      VALUES (?, ?)
+    `);
+    insertTodo.run(defaultTodo, result.lastInsertRowid);
+
+    // create a token
+    const token = jwt.sign(
+      { id: result.lastInsertRowid },
+      process.env.JWT_SECRET,
+      { expiresIn: 86400 },
+    );
+    res.json({ token });
   } catch (err) {
     console.log(err.message);
     res.sendStatus(503);
