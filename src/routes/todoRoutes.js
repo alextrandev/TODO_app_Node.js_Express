8119c90cf698a1f69f691304cd7774f6428ed00e
Alex Tran <tranduclong092@gmail.com>
2024-12-01 10:43:33 +0700

    Feat: fetch the todos list adter user logged in

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -3,8 +3,10 @@ import db from '../db.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  // show the todo list
+router.get('/', (req, res) => {
+  const getTotos = db.prepare('SELECT * FROM todos WHERE user_id = ?');
+  const todos = getTotos.all(req.userId);
+  res.json(todos);
 });
 
 router.post('/', async (req, res) => {
