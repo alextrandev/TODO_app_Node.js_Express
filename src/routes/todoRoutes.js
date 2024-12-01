fb17decb900393fb4a94418a509811c9f6d0dd5f
Alex Tran <tranduclong092@gmail.com>
2024-12-01 15:54:57 +0700

    Feat: add routes for adding new todo task

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -3,14 +3,32 @@ import db from '../db.js';
 
 const router = express.Router();
 
+// get all todos
 router.get('/', (req, res) => {
   const getTotos = db.prepare('SELECT * FROM todos WHERE user_id = ?');
   const todos = getTotos.all(req.userId);
   res.json(todos);
 });
 
+// add new todo task
 router.post('/', async (req, res) => {
-  // add new todo task
+  const { body: {task: task}, userId } = req;
+
+  const insertTodo = db.prepare(`
+    INSERT INTO todos (task, user_id)
+    VALUES (?, ?)
+  `);
+  const result = insertTodo.run(task, userId);
+
+  if (result.changes === 0) {
+    return res.status(500).json({ error: 'Failed to add new todo' });
+  }
+
+  return res.json({
+    id: result.lastInsertRowid,
+    task,
+    completed: false
+  });
 });
 
 router.put('/:id', async (req, res) => {
