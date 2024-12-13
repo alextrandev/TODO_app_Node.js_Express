f6a2eda7558f7560f0431411809ece0eb72a1ff1
Alex Tran <tranduclong092@gmail.com>
2024-12-13 14:25:35 +0700

    Refactor: use prisma for get and create todo in todo enpoints

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import db from '../db.js';
+import prisma from '../prismaClient.js';
 
 const router = express.Router();
 
 // get all todos
-router.get('/', (req, res) => {
-  const getTotos = db.prepare('SELECT * FROM todos WHERE user_id = ?');
-  const todos = getTotos.all(req.userId);
+router.get('/', async (req, res) => {
+  const todos = await prisma.todo.findMany({
+    where: {
+      userId: req.userId
+    }
+  })
+
   res.json(todos);
 });
 
@@ -14,20 +19,21 @@ router.get('/', (req, res) => {
 router.post('/', async (req, res) => {
   const { body: {task: task}, userId } = req;
 
-  const insertTodo = db.prepare(`
-    INSERT INTO todos (task, user_id)
-    VALUES (?, ?)
-  `);
-  const result = insertTodo.run(task, userId);
+  const newTodo = await prisma.todo.create({
+    data: {
+      task: task,
+      userId: userId
+    }
+  });
 
-  if (result.changes === 0) {
+  if (!newTodo) {
     return res.status(500).json({ error: 'Failed to add new todo' });
   }
 
   return res.json({
-    id: result.lastInsertRowid,
+    id: newTodo.id,
     task,
-    completed: false
+    completed: newTodo.completed
   });
 });
 
